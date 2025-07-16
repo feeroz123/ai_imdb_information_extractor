e557d5164da52df8e1a648863062894da4ba7c80
feeroz123 <feeroz123@gmail.com>
2025-07-16 10:23:30 +0530

    added Readme

--- main.py
+++ main.py
@@ -28,16 +28,3 @@ if st.button("Extract Data"):
             st.error("Failed to extract data. Please check the URL and try again.")
     else:
         st.warning("Please enter a valid IMDB movie URL.")
-        
-
-# Make sure to have the data_extractor.py file in the same directory with the extract function
-
-# You can install the required libraries using pip:
-# pip install streamlit pandas spacy
-
-# Make sure to have a '.env' file with the GROQ_API_KEY variable defined in it
-
-# Run this app with:
-# streamlit run main.py
-
-# Test with a IMDB movie url. E.g. https://www.imdb.com/title/tt31216548/?ref_=nv_srb_trend_title_4
